264eff6e7439a758955a6ff98f04ec72fb48c2c6
Simone Morella <simone.morella@ah.nl>
2025-12-02 13:02:46 +0100

    solution day 1: removed comment

--- Day 1/Secret Entrance/solution.js
+++ Day 1/Secret Entrance/solution.js
@@ -1,14 +1,14 @@
 const fs = require('node:fs');
 
 const data = fs.readFileSync('data.txt', 'utf8');
-const lines = data.split('\n'); // or /\r?\n/ for Windows-safe splitting
+const lines = data.split('\n');
 
 function findSecretNumber(lines) {
     let position = 50;
     let numberOfZeros = 0;
     for (const step of lines) {
         const [direction, ...turnsStr] = step;
-        const turns = parseInt(turnsStr.join(''), 10);
+        const turns = Number.parseInt(turnsStr.join(''), 10);
         const towards = direction === 'L' ? -turns : +turns;
         const modulePos = (position + towards + 10000) % 100;
 
