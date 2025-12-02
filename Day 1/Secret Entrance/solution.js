bb33e594c88f9b492b024e98cf47b23b7fba063f
Simone Morella <simone.morella@ah.nl>
2025-12-02 12:24:39 +0100

    solution day 1

---
+++ Day 1/Secret Entrance/solution.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs');
+
+const data = fs.readFileSync('data.txt', 'utf8');
+const lines = data.split('\n'); // or /\r?\n/ for Windows-safe splitting
+
+function findSecretNumber(lines) {
+    let position = 50;
+    let numberOfZeros = 0;
+    for (const step of lines) {
+        const [direction, ...turnsStr] = step;
+        const turns = parseInt(turnsStr.join(''), 10);
+        const towards = direction === 'L' ? -turns : +turns;
+        const modulePos = (position + towards + 10000) % 100;
+
+        if (modulePos === 0) numberOfZeros++;
+
+        position = modulePos;
+    }
+    return numberOfZeros;
+}
+
+const result = findSecretNumber(lines);
+console.log(`The number of times we landed on 0 is: ${result}`);
